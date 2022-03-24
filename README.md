e6211db86f84f759cf5433c7639358b169da98c7
Guilherme Maciel <guilherme5932.ms@gmail.com>
2022-03-24 04:24:19 -0300

    Learning Laravel

--- README.md
+++ README.md
@@ -97,3 +97,113 @@ Route::get('/', function () {
 ```
 {{-- COMENTÁRIO BLADE --}}
 ```
+
+## LAYOUT COM BLADE
+
+- Reaproveitamento de código para evitar repetições.
+
+1. Criar pasta `layouts`
+2. Adicionar arquivo `main.blade.php`
+```html
+<!-- main.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        @yield('content')
+
+    <footer>
+        <p>Guizo &copy; 2022</p>
+    </footer>
+    
+    </body>
+</html>
+```
+:notebook: @yield() permite que o conteúdo se realoque no template main.
+- `@yield('content')`: todo o conteúdo da página ficará aqui, de forma dinâmica.
+- `@yield('title')`: Muda o título dinâmicamente 
+
+Nas Views, você pode remover as tags <head>, <body> e <html>, deixando apenas o conteúdo em si.
+
+```html
+{{-- Define o uso do template MAIN --}}
+@extends('layouts.main')
+
+{{-- Seções que possuem referências nos parâmetros <b>yield</b> --}}
+@section('title', 'Guizo - Welcome')
+@section('content')
+
+{{-- Conteúdo da página --}}
+<nav>
+    <div class="links">
+        <span><h1>USO DE ROTAS: </h1></span>
+        <a href="/products">PRODUCTS</a>
+        <a href="/contact">CONTACT</a>
+    </div>
+</nav>
+<h1>TESTANDO ENGINE BLADE</h1>
+<ul>
+@if(10 > 5)
+    <li>TESTANDO IFs</li>
+@endif
+<li>{{ $data }}</li>
+
+@if($var == "")
+    <li>Variável vazia</li>
+@else 
+    <li>Variável contém dados</li>
+@endif
+<!-- O nome dos dados dinâmicos é a chave do array, e não a variável PHP-->
+</li>
+</ul>
+<h1>USO DE ESTRUTURA DE REPETIÇÃO FOR</h1>
+<h2>FOR</h2>
+<ul>
+    @for($i = 0; $i < count($arr); $i++)
+        <li>{{$arr[$i]}}</li>
+    @endfor
+</ul>
+<h2>FOR EACH</h2>
+<ul>
+    @foreach($nomes as $nome)
+        <li>{{$loop->index}} - {{$nome}}</li>
+    @endforeach
+</ul>
+<h1>UTILIZANDO PHP PURO</h1>
+@php
+    $name = "Exemplo de PHP puro";
+    echo $name;
+@endphp
+
+{{-- Comentário não aparece na tela --}}
+<h1>ARQUIVO ESTÁTICO</h1>
+<img src="/img/hachiman.jpg" alt="hachiman" height="100xp" style="border-radius:50%">
+
+@endsection
+```
+
+# ARQUIVOS ESTÁTICOS
+
+- CSS, JavaScript, imagens...
+
+`public`: Pasta de acesso direto aos arquivos estáticos.
+
+- Basta criar uma pasta e acessá-la nas views.
+
+```html
+<link rel="stylesheet" href="/css/styles.css">
+```
+
+`/css`: acessa a `public/css` 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,111 +1,52 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'Guizo - Welcome')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+@section('content')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-            
-            .full-height {
-                height: 100vh;
-            }
+<nav>
+    <div class="links">
+        <span><h1>USO DE ROTAS: </h1></span>
+        <a href="/products">PRODUCTS</a>
+        <a href="/contact">CONTACT</a>
+    </div>
+</nav>
+<h1>TESTANDO ENGINE BLADE</h1>
+<ul>
+@if(10 > 5)
+    <li>TESTANDO IFs</li>
+@endif
+<li>{{ $data }}</li>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+@if($var == "")
+    <li>Variável vazia</li>
+@else 
+    <li>Variável contém dados</li>
+@endif
+<!-- O nome dos dados dinâmicos é a chave do array, e não a variável PHP-->
+</li>
+</ul>
+<h1>USO DE ESTRUTURA DE REPETIÇÃO FOR</h1>
+<h2>FOR</h2>
+<ul>
+    @for($i = 0; $i < count($arr); $i++)
+        <li>{{$arr[$i]}}</li>
+    @endfor
+</ul>
+<h2>FOR EACH</h2>
+<ul>
+    @foreach($nomes as $nome)
+        <li>{{$loop->index}} - {{$nome}}</li>
+    @endforeach
+</ul>
+<h1>UTILIZANDO PHP PURO</h1>
+@php
+    $name = "Exemplo de PHP puro";
+    echo $name;
+@endphp
 
-            .position-ref {
-                position: relative;
-            }
+{{-- Comentário não aparece na tela --}}
+<h1>ARQUIVO ESTÁTICO</h1>
+<img src="/img/hachiman.jpg" alt="hachiman" height="100xp" style="border-radius:50%">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <nav>
-            <div class="links">
-                <span><h1>USO DE ROTAS: </h1></span>
-                <a href="/products">PRODUCTS</a>
-                <a href="/contact">CONTACT</a>
-            </div>
-
-        </nav>
-        <h1>TESTANDO ENGINE BLADE</h1>
-        <ul>
-        @if(10 > 5)
-            <li>TESTANDO IFs</li>
-        @endif
-        <li>{{ $data }}</li>
-
-        @if($var == "")
-            <li>Variável vazia</li>
-        @else 
-            <li>Variável contém dados</li>
-        @endif
-        <!-- O nome dos dados dinâmicos é a chave do array, e não a variável PHP-->
-        </li>
-        </ul>
-        <h1>USO DE ESTRUTURA DE REPETIÇÃO FOR</h1>
-        <h2>FOR</h2>
-        <ul>
-            @for($i = 0; $i < count($arr); $i++)
-                <li>{{$arr[$i]}}</li>
-            @endfor
-        </ul>
-        <h2>FOR EACH</h2>
-        <ul>
-            @foreach($nomes as $nome)
-                <li>{{$loop->index}} - {{$nome}}</li>
-            @endforeach
-        </ul>
-        <h1>UTILIZANDO PHP PURO</h1>
-        @php
-            $name = "Exemplo de PHP puro";
-            echo $name;
-        @endphp
-
-        {{-- Comentário não aparece na tela --}}
-    </body>
-</html>
+@endsection
